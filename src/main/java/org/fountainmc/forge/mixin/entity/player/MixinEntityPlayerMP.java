672727457102b472b8835517889363c320e83c11
PizzaCrust <torchprojectserver@gmail.com>
2016-06-17 01:38:32 +0700

    Re-added the removed items.
    This commit adds the stuff I accidentally removed.

--- src/main/java/org/fountainmc/forge/mixin/entity/player/MixinEntityPlayerMP.java
+++ src/main/java/org/fountainmc/forge/mixin/entity/player/MixinEntityPlayerMP.java
@@ -24,13 +24,39 @@
 
 package org.fountainmc.forge.mixin.entity.player;
 
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.text.TextComponentString;
 import org.fountainmc.api.entity.Player;
+import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(net.minecraft.entity.player.EntityPlayerMP.class)
 public abstract class MixinEntityPlayerMP implements Player, ICommandSender {
+
+    @Shadow
+    public abstract String shadow$getName();
+
+    @Shadow
+    public abstract UUID shadow$getUniqueID();
+
+    @Intrinsic
+    @Nonnull
+    @Override
+    public String getName() {
+        return this.shadow$getName();
+    }
+
+    @Intrinsic
+    @Nonnull
+    @Override
+    public UUID getUUID() {
+        return this.shadow$getUniqueID();
+    }
 
     @Override
     public void sendMessage(String s) {
